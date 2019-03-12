961b6df2cddaba4ef0a4a074fb14407cb5abf16a
山田 俊輔 <yshunske@gmail.com>
2019-03-12 23:42:45 +0900

    wip:webhookを受けたあとの処理

--- githubwebhook_notify_merge_commit_names/parse_git_hook.rb
+++ githubwebhook_notify_merge_commit_names/parse_git_hook.rb
@@ -3,8 +3,16 @@ require 'json'
 require 'slack-notifier'
 
 def lambda_handler(event:, context:)
-    res = JSON.parse(event['body'])
-    puts res
+    req = JSON.parse(event['body'])
+    puts req
+    res = 'ok'
+    if req['action'] == 'labeld'
+      req['pull_request']['labels'].any? do |label|
+        # TODO: labelフォーマットを確認後に特定のラベルが存在するかをチェック
+      end
+    end
+
+    puts req
     notifier = Slack::Notifier.new ENV['SLACK_NOTIFY_URL']
     notifier.ping "Hello World"
     { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
