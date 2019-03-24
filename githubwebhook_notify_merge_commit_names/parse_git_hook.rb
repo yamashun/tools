5c7f5f1e234d91f6694e3665cc92e328397ab272
山田 俊輔 <yshunske@gmail.com>
2019-03-25 00:45:46 +0900

    全てのcommitsを一度のリクエストで取得できない場合にLinkヘッダーから次の情報を取得するためのurlを取得して繰り返し情報をcommitの情報を取得する

--- githubwebhook_notify_merge_commit_names/parse_git_hook.rb
+++ githubwebhook_notify_merge_commit_names/parse_git_hook.rb
@@ -5,7 +5,6 @@ require 'faraday'
 require 'openssl'
 require 'rack'
 
-# commit 23
 def lambda_handler(event:, context:)
     return { statusCode: 400, body: 'invalid token' } unless verify_signature(event)
     
@@ -15,12 +14,7 @@ def lambda_handler(event:, context:)
 
       begin
         commits_url = req['pull_request']['commits_url']
-        commits = JSON.parse(Faraday.get(commits_url).body)
-        messages = commits.select do |commit|
-          commit['commit']['message'].include?('Merge pull request ') 
-        end.map do |commit|
-          commit['commit']['message'].gsub('Merge pull request ', '').gsub("\n", ' ')
-        end
+        messages = filtered_commits_messages(commits_url)
         unless messages.empty?
           messages.unshift("<!here>", "リリースを開始します。修正内容を確認してください。")
           notifier = Slack::Notifier.new ENV['SLACK_NOTIFY_URL']
@@ -38,4 +32,47 @@ end
 def verify_signature(event)
   signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], event['body'].to_json)
   Rack::Utils.secure_compare(signature, event['headers']['X-Hub-Signature'])
+end
+
+def filtered_commits_messages(commits_url)
+  messages = []
+  request_times = 0
+  
+  # @notice request_times < 10 は想定外の挙動をした場合に無限ループとならないようにするため
+  while !(commits_url.nil? || commits_url.empty?) && request_times < 10 do
+    res = Faraday.get(commits_url)
+    commits = JSON.parse(res.body)
+    messages.concat(merge_commit_messages(commits))
+    request_times += 1
+    
+    commits_url = next_link(res.headers['Link'])
+  end
+  messages
+end
+
+def merge_commit_messages(commits)
+  commits.select do |commit|
+    commit['commit']['message'].include?('Merge pull request ') 
+  end.map do |commit|
+    commit['commit']['message'].gsub('Merge pull request ', '').gsub("\n", ' ')
+  end
+end
+
+# parse headers['Link'] and select next link
+# @params header_link [String]
+#   ex) <https://api.github.com/repositories/11111111111/pulls/1/commits?page=2>; rel="next", <https://api.github.com/repositories/11111111111/pulls/1/commits?page=2>; rel="last"
+# @return String ページングされた次のcommitsの情報を取得するためのapiのurl
+#   ex) https://api.github.com/repositories/11111111111/pulls/1/commits?page=2
+def next_link(header_link)
+  rels = header_link.split(',')
+  url = ''
+
+  rels.each do |rel, index|
+    rel_items = rel.split(';')
+    if rel_items[1][/rel="(.*)"/,1] == 'next'
+      url = rel_items[0][/<(.*)>/,1]
+      break
+    end
+  end
+  url
 end
