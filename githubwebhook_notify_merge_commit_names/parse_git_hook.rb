d8d35e6c9c80e6a98fe3a457d1b29f5731aab00e
山田 俊輔 <yshunske@gmail.com>
2019-03-24 23:13:26 +0900

    githubapi commits_urlテストのためのcommit18

--- githubwebhook_notify_merge_commit_names/parse_git_hook.rb
+++ githubwebhook_notify_merge_commit_names/parse_git_hook.rb
@@ -5,7 +5,7 @@ require 'faraday'
 require 'openssl'
 require 'rack'
 
-# commit 17
+# commit 18
 def lambda_handler(event:, context:)
     return { statusCode: 400, body: 'invalid token' } unless verify_signature(event)
     
